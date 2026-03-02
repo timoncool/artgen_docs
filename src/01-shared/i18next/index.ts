1a0695bb85b8409dc8a7cfa8bb14b24a1e3caf75
Vitalii Siziakov <vitalii.siziakov@gmail.com>
2026-03-02 22:30:14 +0300

    fix: languages

--- src/01-shared/i18next/index.ts
+++ src/01-shared/i18next/index.ts
@@ -1,6 +1,5 @@
 import { getIsSupportedPathLng } from './lib/get-is-supported-path-lng';
 import { getLngPathSegmentServer } from './lib/get-lng-path-segment-server';
-import i18next from './lib/i18next';
 import { fallback_lng, header_lng_param_name, lngs } from './lib/i18next-settings';
 
-export { getIsSupportedPathLng, getLngPathSegmentServer, i18next, fallback_lng, header_lng_param_name, lngs };
+export { getIsSupportedPathLng, getLngPathSegmentServer, fallback_lng, header_lng_param_name, lngs };
